617f81d14837cb573de95fc2906662c046df3d5d
Andre Luiz <adventureandre@hotmail.com>
2024-03-31 12:10:50 -0300

    dicas e  obs

--- clientes.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO vendas_api.clientes (name) VALUES('andre luiz');
-INSERT INTO vendas_api.clientes (name) VALUES ('João');
-INSERT INTO vendas_api.clientes (name) VALUES ('Maria');
-INSERT INTO vendas_api.clientes (name) VALUES ('Pedro');
-INSERT INTO vendas_api.clientes (name) VALUES ('Ana');
-INSERT INTO vendas_api.clientes (name) VALUES ('Lucas');
